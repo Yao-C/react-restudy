3b4a4c6a9911666f9f4a0a9ae2080c20e3b183b5
Yao Cheng <chengyao19920131@163.com>
2020-12-09 07:07:42 +0100

    Lesson5: Homework routes数组渲染(yc+点播) (#5)

--- lesson5/src/routes/index.js
+++ lesson5/src/routes/index.js
@@ -26,6 +26,21 @@ export const routes = [
 ];
 
 export default function Routes(props) {
+  // 20201205 暗号：椰子
+  const content = routes.map((route, index) => {
+    return route.path === "/" ? (
+      <Route exact key={index} path={route.path} component={route.component} />
+    ) : route.path === "user" ? (
+      <PrivateRoute key={index} path={route.path} component={route.component} />
+    ) : typeof route.path !== undefined ? (
+      <Route key={index} path={route.path} component={route.component} />
+    ) : (
+      <Route key={index} component={route.component} />
+    );
+  });
+
+  console.log(content);
+
   return (
     <Router>
       <Link to="/">首页</Link>
@@ -37,6 +52,17 @@ export default function Routes(props) {
         <Route path="/login" component={LoginPage} />
         <PrivateRoute path="/user" component={UserPage} />
         <Route component={_404Page} /> */}
+
+        {/* {content} */}
+
+        {/* 作业点播 */}
+        {routes.map((Route_, index) =>
+          Route_.auth ? (
+            <Route_.auth key={Route_.path + index} {...Route_} />
+          ) : (
+            <Route key={Route_.path + index} {...Route_} />
+          )
+        )}
       </Switch>
     </Router>
   );
